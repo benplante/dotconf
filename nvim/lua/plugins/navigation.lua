a1e6223c33aa01f4204ec5c70ab87c0d1915abae
Ben Plante <ben.plante123@gmail.com>
2023-03-28 18:03:29 -0400

    Combine telescope.lua and neotree.lua into navigation.lua

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -1,5 +1,42 @@
 return {
   {
+    'nvim-neo-tree/neo-tree.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim'
+    },
+    cmd = "Neotree",
+    keys = {
+      { '<leader>n',function() require('neo-tree.command').execute({ toggle = true, dir = vim.loop.cwd() }) end, desc = 'NeoTree Files' }
+    },
+    deactivate = function()
+      vim.cmd [[ Neotree close ]]
+    end,
+    init = function()
+      vim.g.neo_tree_remove_legacy_commands = 1
+      print(vim.fn.argc())
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("neo-tree")
+        end
+      end
+    end,
+    opts = {
+      window = {
+        position = 'float'
+      },
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = true,
+        use_libuv_file_watcher = true,
+      }
+    }
+  },
+
+
+  {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.1',
     dependencies = {
@@ -8,9 +45,11 @@ return {
       'nvim-telescope/telescope-frecency.nvim',
       'kkharji/sqlite.lua',
       'nvim-telescope/telescope-fzy-native.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
     },
     cmd = 'Telescope',
     keys = {
+      { "-", "<CMD>Telescope find_files<CR>", desc = "Find Files" },
       { "<leader>ff", "<CMD>Telescope find_files<CR>", desc = "Find Files" },
       { "<leader>f/", "<CMD>Telescope live_grep<CR>", desc = "Grep in Files" },
       { "<leader>fr", "<CMD>Telescope frecency<CR>", desc = "Frecent Files" },
@@ -30,12 +69,13 @@ return {
 
       telescope.load_extension('fzy_native')
       telescope.load_extension('frecency')
+      telescope.load_extension("ui-select")
 
       if vim.loop.os_uname().sysname == "Windows_NT" then
         vim.g.sqlite_clib_path = vim.fn.expand("$USERPROFILE") .. "\\.sqlite3\\sqlite3.dll"
       end
 
-      telescope.setup {
+      telescope.setup({
         defaults = {
           mappings = {
             n = {
@@ -51,7 +91,7 @@ return {
             override_file_sorter = true,
           }
         }
-      }
+      })
     end
   },
 }
