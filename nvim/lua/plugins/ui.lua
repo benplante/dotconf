8c817242504e112d07de7d1a76e58eb8209aff86
Ben Plante <ben.plante123@gmail.com>
2023-03-29 16:19:39 -0400

    Add more colorschemes and zen-mode + twilight

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -2,15 +2,37 @@ return {
   {
     "nyoom-engineering/oxocarbon.nvim",
     priority = 1000,
+  },
+  {
+    "NLKNguyen/papercolor-theme",
+    priority = 1000,
+  },
+  {
+    "junegunn/seoul256.vim",
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim"
+  },
+  {
+    "folke/zen-mode.nvim",
+    dependencies = {
+      "folke/twilight.nvim"
+    },
+    keys = {
+      { "<leader>z", function() require('zen-mode').toggle() end, desc = 'Zen Mode' }
+    },
     config = function()
-      vim.opt.background = 'dark'
+      require('zen-mode').setup()
+      require('twilight').setup()
     end
   },
   {
     "wuelnerdotexe/vim-enfocado",
     priority = 1000,
     config = function()
-      vim.g.enfocado_style = "neon"
+      vim.opt.background = 'dark'
+      vim.g.enfocado_style = "nature"
       vim.cmd [[ colorscheme enfocado ]]
     end
   },
